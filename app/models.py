d8691f418cd962e6269c14534c2de15a9e3cb937
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-08 16:38:44 +0300

    Adds Blogs and Comment db Model

--- app/models.py
+++ app/models.py
@@ -6,7 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String)
     email = db.Column(db.String)
-    role_id = db.Column(db.Integer, db.Foreignkey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
@@ -20,6 +21,26 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'Role : {self.name}'
+
+
+class Blog(db.Model):
+    __tablename__ = 'blog'
+    id = db.Column(db.Integer, primary_key = Role)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'Blog {self.title}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Comment {self.content}'
 
 
 
