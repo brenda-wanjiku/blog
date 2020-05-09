3ca1445408167d62d1518c08b10a007a215f8d03
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-09 22:25:45 +0300

    Creates navbar in navbar.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,16 @@
 from ..models import User,Role,Blog,Comment
 from .. import db
+from . import main
+from flask import render_template
 
 
 @main.route('/')
 def index():
+    title = "HOMEPAGE"
     return render_template('index.html')
 
 @main.route('/blog/<blog_id>')
 def blog(blog_id):
     title = "Blogs"
-    blogs = Blog.get_blog(blog_id = blog_id).all()
+    blogs = Blog.get_blog(id = blog_id).all()
     return render_template('blog.html', blogs = blogs)
