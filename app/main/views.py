0ec7a6c365f8ea5e10c9ef65f85017af64e4d716
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-12 00:12:09 +0300

    Writes update_blog function

--- app/main/views.py
+++ app/main/views.py
@@ -2,16 +2,15 @@ from ..models import User,Role,Blog,Comment,Quote, Subscriber
 from .forms import AddBlog, SubscriberForm, AddComment, UpdateProfile
 from .. import db
 from . import main
-from flask import render_template, redirect, url_for,flash
+from flask import render_template, redirect, url_for,flash,request
 from ..requests import get_quotes
 from flask_login import login_required, current_user
 from datetime import datetime
 from ..email import mail_message
 
-
 @main.route('/', methods = ['GET', 'POST'])
 def index():
-    title = "This is Ines"
+    title = "Bluebird"
     blogs = Blog.display_blogs()
     quotes = get_quotes()
     subscriber_form = SubscriberForm()
@@ -69,8 +68,9 @@ def update_pic(user_id):
 @main.route('/blog/<blog_id>')
 def blog(blog_id):
     title = "Blogs"
-    blogs = Blog.query.filter_by(id = blog_id).all()
-    return render_template('blog.html', title = title,blogs = blogs)
+    blog = Blog.query.filter_by(id = blog_id).first()
+    comments = Comment.query.filter_by(blog_id = blog_id).all()
+    return render_template('blog.html', title = title,blog = blog, comments = comments)
 
 
 
@@ -92,13 +92,29 @@ def add_blog(user_id):
         for subscriber in subscribers: 
             mail_message("Welcome to Blog", "email/new_blog", subscriber.email)
         return redirect(url_for('main.index'))
+    
 
         title = "New Blog"
         blogs = Blog.query.all()
         return redirect(url_for('main.blog', title = title, blog_id = new_blog.id))
     return render_template('new_blog.html', form = form, title =title)
 
-
+@main.route('/blog/<blog_id>/update', methods =['GET', 'POST'] )
+@login_required
+def update_blog(blog_id):
+    title = "Update Blog"
+    form = AddBlog()
+    blog = Blog.query.filter_by(id=blog_id).first()
+    form.title.data = blog.title
+    form.description.data = blog.description
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        blog.title = title
+        blog.description = description
+        db.session.commit()
+        return redirect(url_for('main.blog', blog_id = blog_id))
+    return render_template('new_blog.html',form = form, title =title )    
 
 @main.route('/blog/<blog_id>/delete_blog')
 @login_required
@@ -116,21 +132,20 @@ def delete_blog(blog_id):
 def comment(blog_id):
     blogs = Blog.query.filter_by(id = blog_id).first()
     comments = Comment.query.filter_by(id = blog_id).first()
-    return redirect(url_for('main.blog',blogs = blogs, blog_id = blog_id))
+    return redirect(url_for('main.blog',blogs = blogs, blog_id = blog_id, comments = comments))
 
 
-@main.route('/comment/<blog_id>/<username>', methods = ['GET', 'POST'])
-def add_comment(blog_id, username):
+@main.route('/comment/<blog_id>', methods = ['GET', 'POST'])
+def add_comment(blog_id):
     title = "Add Comments"
-    user = User.query.filter_by(username = username).first()
     blog = Blog.query.filter_by(id = blog_id).first()
     form = AddComment()
     if form.validate_on_submit():
         content = form.text.data 
-        new_comment = Comment(content= content)
+        new_comment = Comment(content= content, blog_id = blog_id)
         new_comment.save_comment()
         return redirect(url_for("main.comment", blog_id = blog.id))
-    return render_template("new_comment.html",title = blog.title, user = user, form = form,blog = blog)
+    return render_template("new_comment.html",title = blog.title,form = form,blog = blog)
    
 
 
