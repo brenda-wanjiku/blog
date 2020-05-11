5c9d809ac65c5f50c7ffc60ae6a2838db55c7f8c
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-11 11:15:19 +0300

    Debuggs the new post url in navbar. Url not redirecting to addblog form

--- app/main/views.py
+++ app/main/views.py
@@ -8,17 +8,10 @@ from flask_login import login_required, current_user
 
 @main.route('/')
 def index():
-    subscriber_form = SubscriberForm()
-    if subscriber_form.validate_on_submit():
-        subscriber_email = subscriber_form.email.data
-        new_subscriber = Subscriber(email = subscriber_email)
-        new_subscriber.save_subscriber()
-        mail_message("Welcome to 'Bloggerly'"/"email/welcome_user", new_subscriber.email )
-        return render_template(url_for('main.index', id = id))
     title = "Bloggerly"
     blogs = Blog.display_blogs()
     quotes = get_quotes()
-    return render_template('index.html',subscriber_form = subscriber_form ,title = title, blogs = blogs,quotes = quotes)
+    return render_template('index.html',title = title, blogs = blogs,quotes = quotes)
 
 
 @main.route('/user/<user_id>')
@@ -46,7 +39,6 @@ def update_profile(user_id):
     return render_template('update.html', form = form, title = title)
 
 
-
 @main.route('/blog/<blog_id>')
 def blog(blog_id):
     title = "Blogs"
@@ -54,24 +46,25 @@ def blog(blog_id):
     return render_template('blog.html', title = title,blogs = blogs)
 
 
-@main.route('/blog/<blog_id>/new', methods = ['GET', 'POST'])
+@main.route('/blog/<user_id>/new', methods = ['GET', 'POST'])
 @login_required
-def add_blog(blog_id):
-    title = "Add comment"
+def add_blog(user_id):
     form = AddBlog()
-    new_blog = None
-
+    user = User.query.filter_by(id = user_id).first()
+    if user is None:
+        abort(404)
+    title = "Post new blog"
     if form.validate_on_submit():
         title = form.title.data
         description = form.description.data
-
         new_blog = Blog( title = title, description = description)
         new_blog.save_blog()
 
         title = "New Blog"
         blogs = Blog.query.all()
-        return redirect(url_for('main.blog', title = title, blogs = blogs, id  = blog_id))
+        return redirect(url_for('main.blog', title = title))
     return render_template('new_blog.html', form = form, title =title)
+
 
 
 
